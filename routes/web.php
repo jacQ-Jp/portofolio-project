d30a8f92eec33304fcf931cb22c5cedc8f0fcecf
Hi Jacq! <jacquel@192.168.0.247>
2026-03-30 09:37:51 +0700

    feat: require auth before accessing main portfolio route

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\AuthController;
 
-Route::get('/', [ProjectController::class, 'index'])->name('projects.index');
-
 // Auth
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register.show')->middleware('guest');
 Route::post('/register', [AuthController::class, 'register'])->name('register.store')->middleware('guest');
@@ -13,10 +11,10 @@ Route::get('/login', [AuthController::class, 'showLogin'])->name('login.show')->
 Route::post('/login', [AuthController::class, 'login'])->name('login.store')->middleware('guest');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
 
-// Projects (public)
-
 // Projects (protected)
 Route::middleware('auth')->group(function () {
+    Route::get('/', [ProjectController::class, 'index'])->name('projects.index');
+
     Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
     Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
     Route::get('/projects/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
